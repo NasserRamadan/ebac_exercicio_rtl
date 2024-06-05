abda3e8ba44321dbe1c4342e4c864bc688415dfa
Nasser Ramadan <nasserramadan22@gmail.com>
2024-06-05 09:01:53 -0300

    Mudanças para testes

---
+++ src/components/PostComments/__tests__/PostComments.test.tsx
@@ -0,0 +1,28 @@
+import { fireEvent, render, screen } from '@testing-library/react';
+import Post from '..';
+import PostComment from '..';
+
+describe('Teste para o componente PostComment', () => {
+    test('Deve renderizar o componente corretamente', () => {
+        render(<PostComment/>);
+        expect(screen.getByTestId('comment-button')).toBeInTheDocument();
+    })
+
+    test('Deve inserir dois comentários', () => {
+        render(<PostComment />)
+
+        const textarea = screen.getByTestId('comment-textarea');
+        const button = screen.getByTestId('comment-button');
+
+        fireEvent.change(textarea, { target: { value: 'Primeiro comentário' } });
+        fireEvent.click(button);
+
+        fireEvent.change(textarea, { target: { value: 'Segundo comentário' } });
+        fireEvent.click(button);
+
+        const comments = screen.getAllByTestId('comentario-rlz');
+        expect(comments).toHaveLength(2);
+        expect(comments[0]).toHaveTextContent('Primeiro comentário');
+        expect(comments[1]).toHaveTextContent('Segundo comentário');
+    });
+});
